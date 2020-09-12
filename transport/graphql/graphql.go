ebc0c4dd32f51bc95b6f800090695231e3ee8df2
Smith Samuel Aitufe <smithaitufe@live.com>
2020-09-12 22:25:46 +0100

    fixed references to todo usecase

--- transport/graphql/graphql.go
+++ transport/graphql/graphql.go
@@ -3,15 +3,15 @@ package graphql
 import (
 	"github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
-	"github.com/smithaitufe/go-todo"
 	"github.com/smithaitufe/go-todo/transport/graphql/schema"
+	"github.com/smithaitufe/go-todo/usecase"
 )
 
 type graphqlServer struct {
-	todoUsecase todo.TodoUsecase
+	todoUsecase usecase.TodoUsecase
 }
 
-func NewGraphQLServer(todoUsecase todo.TodoUsecase) *relay.Handler {
+func NewGraphQLServer(todoUsecase usecase.TodoUsecase) *relay.Handler {
 	s := schema.GetRootSchema()
 	opts := []graphql.SchemaOpt{graphql.UseFieldResolvers()}
 	parsedSchema := graphql.MustParseSchema(s, &graphqlServer{todoUsecase}, opts...)
