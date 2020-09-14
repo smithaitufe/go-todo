6ffc9b43484af09b8a788e7e442215afc6f0f0bc
Smith Samuel Aitufe <smithaitufe@live.com>
2020-09-14 23:01:56 +0100

    added functionality to user usecase implementation

--- usecase/user.go
+++ usecase/user.go
@@ -8,10 +8,45 @@ type UserUsecase interface {
 	GetUsers() ([]todo.User, error)
 	GetUser(id uint) (*todo.User, error)
 	CreateUser(u todo.User) (*todo.User, error)
-	UpdateUser(id uint, u *todo.User) (*todo.User, error)
+	UpdateUser(id uint, u todo.User) (*todo.User, error)
 	DeleteUser(id uint) error
 }
 
 type userUsecase struct {
 	userRepo todo.UserRepository
+}
+
+func NewUserUsecase(ur todo.UserRepository) UserUsecase {
+	return &userUsecase{ur}
+}
+
+func (u *userUsecase) GetUsers() ([]todo.User, error) {
+	o, err := u.userRepo.FindUsers()
+	if err != nil {
+		return nil, err
+	}
+	return o, nil
+}
+func (u *userUsecase) GetUser(id uint) (*todo.User, error) {
+	return u.userRepo.FindUser(id)
+}
+func (u *userUsecase) CreateUser(in todo.User) (*todo.User, error) {
+	return u.userRepo.CreateUser(in)
+}
+
+func (u *userUsecase) UpdateUser(id uint, in todo.User) (*todo.User, error) {
+	o, err := u.userRepo.FindUser(id)
+	if err != nil {
+		return nil, err
+	}
+	in.ID = o.ID
+	return u.userRepo.UpdateUser(in)
+}
+
+func (u *userUsecase) DeleteUser(id uint) error {
+	o, err := u.userRepo.FindUser(id)
+	if err != nil {
+		return err
+	}
+	return u.userRepo.DeleteUser(*o)
 }
